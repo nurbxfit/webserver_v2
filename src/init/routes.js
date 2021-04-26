80229121ce2f28f3915e3abc9f2c0a8338f2c919
nurbxfit <redcarbon97@gmail.com>
2021-04-26 15:45:54 +0800

    update: init->routes ; removes commented code

--- src/init/routes.js
+++ src/init/routes.js
@@ -6,15 +6,9 @@ const {
 } = require('../middlewares/PageNotFound');
 
 module.exports = function(app){
-    // app.use('/',require('../routes/index'));
     app.use('/admin',require('../routes/admin'));
-    // app.use('/auth', require('../routes/authentication'));
     app.use('/user',require('../routes/user'));
     app.use('/auth',require('../routes/authentication'));
-    // app.use('/article',require('../routes/article'));
-    // app.use('/annotation',require('../routes/annotation'));
-    // app.use('/consultation',require('../routes/consultation'));
     app.use(errorLogger);
-    // app.use(genericError);
     app.use(pageNotFound);
 }
