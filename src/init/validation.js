d3b2362284193308de35790018f4d5a496fb44d1
nurbxfit <redcarbon97@gmail.com>
2021-04-26 15:49:25 +0800

    comment something

---
+++ src/init/validation.js
@@ -0,0 +1,5 @@
+/*
+Decide to put Joi validation Schema here, or anything related to validation,
+for easy access and configuration or maintain.
+but I don't know, don't have ideas, too lazy. instead I put validation as methods in models or in controller
+*/
